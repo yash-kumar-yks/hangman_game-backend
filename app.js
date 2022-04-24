2264ca8eafe7b767dab56e91b69d2e78af9fee04
yash <yashsngh57@gmail.com>
2022-04-24 20:47:08 +0530

    adding sessions

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const Express = require("express");
 const Router = require("./routes");
-
+const { sequelize, Word } = require("./models");
+const res = require("express/lib/response");
+require("dotenv").config();
 
 async function initialize() {
   const app = Express();
   app.use(Express.json());
+  app.use((req,res,next) =>{
+    res.set("Access-Control-Allow-Origin","https://imaginative-mochi-a26431.netlify.app");
+    res.set("Access-Control-Allow-Headers","Content-type");
+    next();
+  })
   // app.get('/', (res,req) =>{
-  //   res.send("hello");
+  //   req.send("hello");
   // })
   app.use("/api", Router);
-  app.listen(8000, () => {
-    console.log("Running application on port 8000");
+  await sequelize.sync();
+  await Word.bulkCreate([
+    {title:"bazzinga"},
+    {
+      title:"house"},
+    {
+      title:"sheldon"
+    }
+  ])
+  app.listen(process.env.PORT || 7000, () => {
+    console.log('Running application on port 7000}');
   });
 }
 
--- routes/sessions/controller.js
+++ routes/sessions/controller.js
@@ -1,25 +1,33 @@
-
+const Sequelize = require("sequelize");
+const { Word, GameSession, sequelize } = require("../../models");
+const serializeGameSession = require("../../serializers/gameSession");
+const gameSessionService = require("../../services/game_session_service");
 async function Createsession(req, res) {
   const name = req.body.name;
-  
-
-  
-
-  res.json({
-      name:name,
-      id:"123"
+  const word = await Word.findOne({
+    order: sequelize.random(),
   });
+
+  //const word = await Word.findOne();
+
+  const gameSession = await GameSession.create({
+    Player_name: name,
+    Played_letters: "",
+    wordId: word.id,
+    StartedAt: new Date(),
+  });
+
+  res.json(await serializeGameSession(gameSession));
 }
 
 async function Playsession(req, res) {
   const gameID = req.params.id;
   const letter = req.body.letter;
-  
-  res.json({
-    "id":gameID,
-    "letter":letter
+  const gameSession = await GameSession.findByPk(gameID);
 
-})
+  await gameSessionService.playWordInGameSession(gameSession, letter);
+
+  res.json(await serializeGameSession(gameSession));
 }
 
 module.exports = {
